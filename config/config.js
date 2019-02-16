027a7461ce570a9c4de561a79f71dd5bde9752a4
MistaWizard <tylermckellar@gmail.com>
2019-02-16 11:49:20 -0500

    Added .env security measures

---
+++ config/config.js
@@ -0,0 +1,22 @@
+module.exports =
+{
+  "development": {
+    "username": "root",
+    "password": process.env.MYSQL_PWD,
+    "database": "burgers_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "testdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
