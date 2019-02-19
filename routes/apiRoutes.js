0ba7e5146e6cf63d4b2c750c5c8ab111601d26da
MistaWizard <tylermckellar@gmail.com>
2019-02-18 22:21:23 -0500

    Api routes for getting data in and out of DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,52 @@ const db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/heroes", function(req, res) {
+    db.Heroes.findAll({}).then((results) => {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/villains", function(req, res) {
+    db.Villains.findAll({}).then((results) => {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/heroes/:id" , function(req, res) {
+    db.Heroes.findOne({ where: { id: req.params.id } }).then((results) => {
+      res.json(results);
+    });
+  });
+  
+  app.get("/api/villains/:id" , function(req, res) {
+    db.Villains.findOne({ where: { id: req.params.id } }).then((results) => {
+      res.json(results);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/heroes", function(req, res) {
+    db.Heroes.create({
+      name: req.body.name,
+      secretIdentity: req.body.secretIdentity,
+      earlyLife: req.body.earlyLife,
+      currentHistory: req.body.currentHistory,
+      personalLife: req.body.personalLife
+    }).then((results) => {
+      res.json(results);
+    });
+  });
+
+  app.post("/api/villains", function(req, res) {
+    db.Villains.create({
+      name: req.body.name,
+      secretIdentity: req.body.secretIdentity,
+      earlyLife: req.body.earlyLife,
+      currentHistory: req.body.currentHistory,
+      personalLife: req.body.personalLife
+    }).then((results) => {
+      res.json(results);
     });
   });
 
