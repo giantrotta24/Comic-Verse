8a192112bdc2bbab6e802f08a7b8c48c6f832ef6
DrewDrizzle <aramouni8@gmail.com>
2019-02-21 20:50:32 -0500

    fixed password and casings

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
-INSERT INTO userName (name, userName, location, admin) VALUES ('Tyler McKellar', 'MistaWizard', 'Ohio', true);
--- used user but said user is wrong syntax
+-- INSERT INTO user (name, userName, location, admin) VALUES ('Tyler McKellar', 'MistaWizard', 'Ohio', true);
+-- -- used user but said user is wrong syntax
